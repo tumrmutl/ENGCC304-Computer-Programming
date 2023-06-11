acd0acaef19a57b6ff5ede5a5b9775a42a81506f
der-way-dark <mr.kittinan@gmail.com>
2023-06-11 17:00:50 +0700

    complete hw1-2

--- Homework/hw1-2-fix-bug.cpp
+++ Homework/hw1-2-fix-bug.cpp
@@ -1,4 +1,8 @@
-#include <stdio.h>
-int main() {
-    return 0 ;
-}
+//TODO: #6 Fix all bugs.
+#include <studio.h>
+int main() 
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) 
+    printf( "This is my first time to coding with C language.\n" ) ;
+    print( "Today is my birthday.\n" ) 
+    printf( "Say \"Happy Birthday\" to me." ) ;
+    return '0' ;
