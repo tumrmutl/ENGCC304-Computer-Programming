1838a83cd0e942c9b1fe7277a7a9b4db9eafad4f
der-way-dark <mr.kittinan@gmail.com>
2023-06-11 15:32:23 +0700

    add todo in hw1-1

--- Homework/hw1-1-helloworld.cpp
+++ Homework/hw1-1-helloworld.cpp
@@ -1,3 +1,5 @@
-//TODO : provide me full syntax of C-Language contain include, main function(int).
-//TODO : display me "Hello World." to me.
-//TODO : Write your private info into Student-Info.txt
+//TODO: #3 provide me full syntax of C-Language contain include, main function(int).
+
+//TODO: #4 display me "Hello World." to me.
+
+//TODO: #5 Write your private info into Student-Info.txt
