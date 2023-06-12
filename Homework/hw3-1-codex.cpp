350ee28a80e9b987adf250628a079494fc18c9ad
der-way-dark <mr.kittinan@gmail.com>
2023-06-12 09:47:34 +0700

    add hw3-1

--- Homework/hw3-1-codex.cpp
+++ Homework/hw3-1-codex.cpp
@@ -1,9 +1,12 @@
-
+//TODO: #8 Arrange this C coding into CODEX pattern.
 #include <stdio.h>
 #include <conio.h>
-
 int main(){
     int a=10;
-    int b=20,c=30,d=40; printf("This is %d + %d = %d",a,b,a+b);
-    return 0;
-}
+int b=20,c=30,d=40; printf("This is %d + %d = %d",a,b,a+b);
+    for(int i=0;i<d+10;i++){
+  if(i<30){
+ printf("Here\n");
+   if(i==25){printf("This is 25.\n");}
+  }printf("ok\n");
+}return 0;}
