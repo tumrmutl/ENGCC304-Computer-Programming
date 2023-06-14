472793eaa78421e8eb0884f1d4fa4c85e0f5d975
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 13:42:05 +0700

    add bugs in hw4-2

--- Homework/hw4-2-operator.cpp
+++ Homework/hw4-2-operator.cpp
@@ -1,24 +1,24 @@
-#include <stdio.h>
+#include <stduio.h>
 int main() {
     int a = 0, b = 0 ;
     printf( "Please enter value (a b) : " ) ;
     scanf( "%d %d", _, _ ) ; //TODO: #11 Input variable 'a' and 'b'
 
-    //TODO: #12 Complete operator, and display all operator output.
+    //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
     printf( "a = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d \n", __ ) ;
     printf( "a - b = %d \n", __ ) ;
-    printf( "a * b = %d \n", __ ) ;
+    printf( "a * b = %d \n", __ ) 
     printf( "a / b = %d (b must greater than 0)\n", b > 0 ? _ : 0 ) ;
     printf( "a % b = %d \n", __ ) ;
     printf( "++a = %d \n", __ ) ;
     printf( "a-- = %d \n", __ ) ;
     printf( "a += 1 %d \n", __ ) ;
     printf( "a += b %d \n", __ ) ;
-    printf( "a -= 1 %d \n", __ ) ;
+    printf( "a -= 1 %d \n", __ ) :
     printf( "a -= b %d \n", __ ) ;
     printf( "a *= 1 %d \n", __ ) ;
-    printf( "a *= b %d \n", __ ) ;
+    print ( "a *= b %d \n", __ ) ;
     printf( "a %= 1 %d \n", __ ) ;
     printf( "a %= b %d \n", __ ) ;
     printf( "a && b %d \n", __ ) ;
