b9783c320ba3fc31abef0c4399bdf069d8146711
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 16:27:42 +0700

    add new lab

---
+++ Homework/hw06-2-roman-number.cpp
@@ -0,0 +1,25 @@
+/*
+    ผู้ใช้กรอกตัวเลขเข้ามาในระบบ และให้คุณแปลงตัวเลขดังกล่าวให้เป็นตัวเลขโรมัน
+    (โจทย์ข้อนี้ให้ใช้ For Loop เท่านั้น)
+
+    Test case:
+        8
+    Output:
+        8 = VIII
+
+    Test case:
+        4
+    Output:
+        4 = IV
+
+    Test case:
+        514
+    Output:
+        514 = DXIV
+
+    Test case:
+        929
+    Output:
+        929 = CMXXIX
+
+*/
