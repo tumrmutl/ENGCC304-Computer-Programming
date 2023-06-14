06f8a1245203a803122022069265e396e2b9c8f1
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 13:40:58 +0700

    complete hw4-2

--- Homework/hw4-2-operator.cpp
+++ Homework/hw4-2-operator.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
 int main() {
-    //--| YOUR CODE HERE
+    int a = 0, b = 0 ;
+    printf( "Please enter value (a b) : " ) ;
+    scanf( "%d %d", _, _ ) ; //TODO: #11 Input variable 'a' and 'b'
 
-    
+    //TODO: #12 Complete operator, and display all operator output.
+    printf( "a = %d , b = %d \n", a, b ) ;
+    printf( "a + b = %d \n", __ ) ;
+    printf( "a - b = %d \n", __ ) ;
+    printf( "a * b = %d \n", __ ) ;
+    printf( "a / b = %d (b must greater than 0)\n", b > 0 ? _ : 0 ) ;
+    printf( "a % b = %d \n", __ ) ;
+    printf( "++a = %d \n", __ ) ;
+    printf( "a-- = %d \n", __ ) ;
+    printf( "a += 1 %d \n", __ ) ;
+    printf( "a += b %d \n", __ ) ;
+    printf( "a -= 1 %d \n", __ ) ;
+    printf( "a -= b %d \n", __ ) ;
+    printf( "a *= 1 %d \n", __ ) ;
+    printf( "a *= b %d \n", __ ) ;
+    printf( "a %= 1 %d \n", __ ) ;
+    printf( "a %= b %d \n", __ ) ;
+    printf( "a && b %d \n", __ ) ;
+    printf( "a || b %d \n", __ ) ;
     return 0 ;
 }
