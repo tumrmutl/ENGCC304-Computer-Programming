eb38c4dcce2fe06aa82f925ed4f8adfcc2119bff
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 13:32:44 +0700

    complete hw4-1

--- Homework/hw4-1-variable.cpp
+++ Homework/hw4-1-variable.cpp
@@ -1,4 +1,9 @@
 #include <stdio.h>
 int main() {
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ;
+    char d = 'a' ;
+    printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
     return 0 ;
 }
--- Homework/hw4-2-operator.cpp
+++ Homework/hw4-2-operator.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
 int main() {
+    //--| YOUR CODE HERE
+
+    
     return 0 ;
 }
