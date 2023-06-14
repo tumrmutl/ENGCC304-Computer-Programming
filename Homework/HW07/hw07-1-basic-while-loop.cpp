e4d47d481596d730f463ac7ed7d3f3f71fa7809c
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 19:24:53 +0700

    add while & do while

---
+++ Homework/HW07/hw07-1-basic-while-loop.cpp
@@ -0,0 +1,32 @@
+/*
+    ผู้ใช้กรอกจำนวนตัวเลข เพื่อพิมพ์คำว่า "Hello World" ตามจำนวนบรรทัดที่ผู้ใช้กรอก
+    หากผู้ใช้กรอกเลขคี่ ให้แสดงเลขบรรทัดโดยใช้ []
+    หากผู้ใช้กรอกเลขคู่ ให้แสดงเลขบรรทัดโดยใช้ ()
+    (โจทย์ข้อนี้ให้ใช้ While Loop เท่านั้น)
+
+    Test case:
+        10
+    Output:
+        (1) Hello World
+        (2) Hello World
+        (3) Hello World
+        (4) Hello World
+        (5) Hello World
+        (6) Hello World
+        (7) Hello World
+        (8) Hello World
+        (9) Hello World
+        (10) Hello World
+
+    Test case:
+        7
+    Output:
+        [1] Hello World
+        [2] Hello World
+        [3] Hello World
+        [4] Hello World
+        [5] Hello World
+        [6] Hello World
+        [7] Hello World
+
+*/
