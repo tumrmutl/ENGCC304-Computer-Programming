06967444006dc04c70e4b74909de2efe4f67a92e
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 13:12:06 +0700

    complete hw3-2

---
+++ Homework/hw3-2-codex-and-fix-bug.cpp
@@ -0,0 +1,8 @@
+//TODO: Fix bug and arrange code in CODEX pattern.
+#include <studio.h>
+#include <conio.h>
+int main(){
+float a,b=3;bool _1=0;printf("%d ",_1);int i=0;
+if(_1!=0)printf("ok ")else while(i<=5) 
+printf(" not-okey-%d ", ++i) for(i=3;i<10;i+=1)if( i%2==(!1?1:2)) printf("see see"): printf("haha");
+return 0;}
