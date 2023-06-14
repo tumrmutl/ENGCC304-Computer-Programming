0cd6eb6b4d6f08f579708de2ed9218cf6979409c
der-way-dark <mr.kittinan@gmail.com>
2023-06-14 14:04:48 +0700

    rename lab1

---
+++ Lab/Lab1-Basic-Display-Input-Output.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char Name[50] ;
+    int  Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s", Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", Age ) ;
+    printf( "- - - - - -\n" ) ;
+    printf( "______ \n", ___ ) ; //TODO: #14 Say hello to user.
+    printf( "______ \n", ___ ) ; //TODO: #15 display user their age number plus 1.
+    return 0 ;
+}
