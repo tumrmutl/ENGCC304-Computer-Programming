8d56511e4c823a2825f310730d554d2e59f6fb72
der-way-dark <mr.kittinan@gmail.com>
2023-06-15 10:43:17 +0700

    add hw string

---
+++ Homework/HW10/hw10-1-basic-string.cpp
@@ -0,0 +1,15 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับค่า String ที่สามารถรับชื่อและนามสกุลไว้ในตัวแปรเดียวกันได้ พร้อมกับแสดงผลบนหน้าจอคอมพิวเตอร์
+
+    Test case:
+        Kittinan Noimanee
+    Output:
+        Name: Kittinan Noimanee
+
+    Test case:
+        Visual Studio Code
+    Output:
+        Name: Visual Studio Code
+
+*/
+
---
+++ Homework/HW10/hw10-2-compare.cpp
@@ -0,0 +1,41 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับค่าอักขระจากผู้ใช้ทีละ 1 ตัวอักษรไปเรื่อยๆ เพื่อรับค่าใส่ไว้ภายในอาเรย์ แต่หากเจอตัวอักษร 'Y' หรือ 'y' ให้หยุดการทำงาน และแสดงค่าภายใน String ทันที
+
+    Test case:
+        Enter 1 character : 
+            U
+        Enter 1 character : 
+            v
+        Enter 1 character : 
+            G
+        Enter 1 character : 
+            k
+        Enter 1 character : 
+            R
+        Enter 1 character : 
+            m
+        Enter 1 character : 
+            q
+        Enter 1 character :
+            Y
+    Output:
+        String = U v G k R m q
+
+    Test case:
+        Enter 1 character : 
+            H
+        Enter 1 character : 
+            e
+        Enter 1 character : 
+            l
+        Enter 1 character : 
+            l
+        Enter 1 character : 
+            o
+        Enter 1 character : 
+            y
+    Output:
+        String = H e l l o
+
+*/
+
