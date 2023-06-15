37020ba020e98064f34648e1ef99ef095a16d2fb
der-way-dark <mr.kittinan@gmail.com>
2023-06-15 12:15:27 +0700

    complete lab 4

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -1,3 +1,30 @@
+/*
+    เขียนโปรแกรมภาษาซีเพื่อรับข้อมูลพนักงานของบริษัทซอร์ฟแวร์ โดยรับข้อมูล รหัสประจำตัวพนักงาน , จำนวนชั่วโมงที่ทำงาน , รายได้ต่อชั่วโมง จากนั้นให้แสดงข้อมูลเลขประจำตัวพนักงาน พร้อมกับรายได้ทั้งหมดที่หนักงานจะได้รับทั้งหมด
+
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0342
+        Input the working hrs: 
+            8
+        Salary amount/hr: 
+            15000
+    Output:
+        Expected Output:
+        Employees ID = 0342
+        Salary = U$ 120000.00
+
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0000500349
+        Input the working hrs: 
+            11
+        Salary amount/hr: 
+            34000
+    Output:
+        Expected Output:
+        Employees ID = 0000500349
+        Salary = U$ 374000.00
+*/
 #include <stdio.h>
 
 int main() {
