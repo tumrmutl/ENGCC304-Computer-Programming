6ef291eb7009c4d9ccb1d0e726a0f252f15aeee5
der-way-dark <mr.kittinan@gmail.com>
2023-06-16 11:30:05 +0700

    complete lab 14-16

--- Lab/Lab15-File.cpp
+++ Lab/Lab15-File.cpp
@@ -1,3 +1,27 @@
+/*
+    จงเขียนฟังก์ชันเพื่ออ่านจำนวนตัวคำภายในไฟล์ (โดยไม่นับรวมเครื่องหมายเว้นวรรค)
+
+    ข้อมูลภายในไฟล์ a.txt
+    Hi, This is my first time in RMUTL.
+
+    Test Case :
+        Enter file name:
+            a.txt
+    Output:
+        Total number of words in 'a.txt' : 8 words
+
+
+
+    ข้อมูลภายในไฟล์ a.txt
+    Encourage your students to test their program with different input files to ensure its correctness.
+
+    Test Case :
+        Enter file name:
+            a.txt
+    Output:
+        Total number of words in 'a.txt' : 15 words
+*/
+
 #include <stdio.h>
 
 int main() {
