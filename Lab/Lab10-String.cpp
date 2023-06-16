26447148efbeee98e983d496b6ba5de2d4a852f2
der-way-dark <mr.kittinan@gmail.com>
2023-06-16 09:48:39 +0700

    complete lab 10 & 11

--- Lab/Lab10-String.cpp
+++ Lab/Lab10-String.cpp
@@ -1,3 +1,38 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับคำจากผู้ใช้งาน เพื่อตรวจสอบว่า คำที่กรอกมามีลักษณะเป็นคำหรือวลีที่สามารถอ่านจากหลังไปหน้าหรือหน้าไปหลังแล้วยังคงความหมายเหมือนเดิมได้
+    โดยที่ หากคำนั้นสามารถอ่านจากหน้าไปหลังหรือหลังไปได้ ให้แสดงผลลัพธ์ว่า Pass แต่หากทำไม่ได้ให้ขึ้นว่า Not Pass
+
+    Test case:
+        Enter word:
+            radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            hello
+    Output:
+        Not Pass.
+
+    Test case:
+        Enter word:
+            Radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            Madam
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            here
+    Output:
+        Not Pass.
+*/
+
 #include <stdio.h>
 
 int main() {
