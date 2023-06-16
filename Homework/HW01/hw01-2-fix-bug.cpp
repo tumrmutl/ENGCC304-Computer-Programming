b72c1ae809ca4cc620d0643440ca9f7551ff8ea9
der-way-dark <mr.kittinan@gmail.com>
2023-06-16 22:35:56 +0700

    hw01-2

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -1,5 +1,5 @@
 //TODO: #6 Fix all bugs.
-#include <studio.h>
+#include <studio .h>
 int main() 
     printf( "Hello, I'm Javis (RMUTL-Version).\n" ) 
     printf( "This is my first time to coding with C language.\n" ) ;
