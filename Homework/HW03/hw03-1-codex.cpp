baff8c40e149b0ab2d5e2f133a487dd2e359b999
der-way-dark <mr.kittinan@gmail.com>
2023-06-17 19:56:17 +0700

    update hw03-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -3,10 +3,10 @@
 #include <conio.h>
 int main(){
     int a=10;
-int b=20,c=30,d=40; printf("This is %d + %d = %d",a,b,a+b);
-    for(int i=0;i<d+10;i++){
+int b=20,c=30,d=40; printf("This is %d + %d = %d ",a,b,a+b);
+    for(int i=0;i<d-30;i++){
   if(i<30){
- printf("Here\n");
-   if(i==25){printf("This is 25.\n");}
-  }printf("ok\n");
+ printf("Here ");
+   if(i==8){printf("This is 8. ");}
+  }printf("ok");
 }return 0;}
