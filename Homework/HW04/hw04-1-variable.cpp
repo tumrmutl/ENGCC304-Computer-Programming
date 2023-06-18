fb0f08fd6dd8b4f6f102f5f862947b996a166b67
der-way-dark <mr.kittinan@gmail.com>
2023-06-18 12:53:44 +0700

    update hw04-1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
-int main() {
-    int a = 10 ;
-    float b = 20 ;
-    char c[] = "Haha" ;
-    char d = 'a' ;
-    printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
-    return 0 ;
-}
+/*
+    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+
+    Output:
+        10 20.0 Haha a
+*/
+
+int a = 10 ;
+float b = 20 ;
+char c[] = "Haha" ;
+char d = 'a' ;
+printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
