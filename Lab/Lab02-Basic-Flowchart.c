7497316ed4d6b95cef675190e14b1ab7bc5f6b0e
Tum RMUTL <mr.kittinan@rmutl.ac.th>
2024-10-04 09:44:44 +0700

    Update and rename Lab02-Basic-Flowchart.drawio to Lab02-Basic-Flowchart.c

---
+++ Lab/Lab02-Basic-Flowchart.c
@@ -0,0 +1,37 @@
+จงเรียนโปรแกรมรับค่าจากผู้ใช้ และทำการแสดงผลบนหน้าจอคอมพิวเตอร์ ให้ได้ผลลัพธ์ดังต่อไปนี้
+
+Testcase #1
+
+Input :
+Enter a character: 
+O
+Enter two integers: 
+14 13
+Enter a floating-point number: 
+89.99082
+Enter a string: 
+Hello
+
+Output :
+You entered character: O
+You entered integers: 14 and 13
+You entered floating-point number: 89.99
+You entered string: Hello
+
+Testcase #2
+
+Input :
+Enter a character: 
+T
+Enter two integers: 
+89 234
+Enter a floating-point number: 
+1.23456
+Enter a string: 
+World
+
+Output :
+You entered character: T
+You entered integers: 89 and 234
+You entered floating-point number: 1.23
+You entered string: World
