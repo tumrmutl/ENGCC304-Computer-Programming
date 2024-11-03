083d8e65e2124cc867202b043e3c16341b0f51da
tumrmutl <mr.kittinan@gmail.com>
2024-11-03 13:50:10 +0700

    update hw & lab

--- Homework/HW01/hw01-2-fix-bug.cpp
+++
@@ -1,7 +0,0 @@
-//TODO: #6 Fix all bugs.
-#include <studio .h>
-int main() 
-    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) 
-    printf( "This is my first time to coding with C language.\n" ) ;
-    print( "Today is my birthday.\n" ) 
-    return '0' ;
